d9ec19d610c00afe12733d2a54f3b302a5d7d916
whoelse <347300979@qq.com>
2024-05-26 12:14:28 +0800

    update : 函数重载

--- TS的函数重载.ts
+++ TS的函数重载.ts
@@ -9,27 +9,31 @@ interface Utils {
   showMessage: ShowMessage;
 }
 
-
 const utils: Utils = {
-  showMessage: (
-    param1: string | object,
-    param2?: string | number | Function,
-    param3?: Function | number
-  ) => {
-  }
-}
-utils.showMessage("")
+  showMessage: (param1: string | object, param2?: string | number | Function, param3?: Function | number) => {}
+};
+utils.showMessage("");
 
+utils.showMessage({
+  mode: "mode",
+  text: "text",
+  onclose: function () {},
+  duration: 3000
+});
+utils.showMessage("text");
+utils.showMessage("text", function () {});
+utils.showMessage("text", "mode");
+utils.showMessage("text", "mode", 3000);
+utils.showMessage('text',3000);
+utils.showMessage('text', 3000, function(){})
 /* function message(options: object): void
 function message(text: string, onClose?: Function): void
 function message(text: string, mode: string, duration?: number): void
 function message(text: string, duration?: number, onClose?: Function): void
  */
 
-function message(param1: string | object,
-  param2?: string | number | Function,
-  param3?: Function | number): void {
-  console.log('param1', param1)
+function message(param1: string | object, param2?: string | number | Function, param3?: Function | number): void {
+  console.log("param1", param1);
 }
 
-message('hello')
+message("hello");
